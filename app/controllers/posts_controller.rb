64e352c2904e10f0afdaa3e83fd81a8663fc111a
Merline Belizaire <merline@noukod.com>
2019-01-29 19:41:07 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,17 +23,38 @@ class PostsController < ApplicationController
 		@article = Article.find(params[:id])
 	end
 
-	def update
-		@article = Article.find(params[:id])
- 		@article.update(title: params[:title], description: params[:description])
- 			redirect_to article_path(@article)
-	end
+	# PATCH/PUT /posts/1
+  # PATCH/PUT /posts/1.json
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
-	def destroy
+  # DELETE /posts/1
+  # DELETE /posts/1.json
+  def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'post was successfully destroyed.' }
+      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def post_params
+      params.require(:post).permit(:title, :description)
+    end
 end
