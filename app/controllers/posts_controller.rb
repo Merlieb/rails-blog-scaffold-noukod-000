f0e98e3343e23fa1dac9dc6166514d520f929172
Merline Belizaire <merline@noukod.com>
2019-01-29 18:20:29 +0000

    Automatically backed up by Learn

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+class PostsController < ApplicationController
+	def index
+		@posts = Post.all
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+	  @post = Post.new
+	  @post.title = params[:title]
+	  @post.description = params[:description]
+	  @post.save
+	  redirect_to post_path(@post)
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+end
+
+def update
+	@article = Article.find(params[:id])
+ @article.update(title: params[:title], description: params[:description])
+ redirect_to article_path(@article)
+end
+end
