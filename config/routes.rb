e476da21327fa642f6146584f6be97caf3f1364b
Merline Belizaire <merline@noukod.com>
2019-01-29 19:46:18 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :apartments
+#  resources :apartments
 #  get 'categories/show'
   resources :posts #, only: [:index, :show, :new, :create, :edit, :update]
   #resources :categories
